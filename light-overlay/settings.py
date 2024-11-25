52f23554f93411ba5536b84ad75f8f6f65faa04b
Dan Biagini <28396789+danbiagini@users.noreply.github.com>
2024-11-25 17:34:00 -0500

    adding more configuration to overlay features (email verification and site_id)

--- light-overlay/settings.py
+++ light-overlay/settings.py
@@ -5,3 +5,21 @@ from cvat.settings.production import *
 
 # Add the $CVAT_HOST to the CSRF_TRUSTED_ORIGINS
 CSRF_TRUSTED_ORIGINS = [f"https://{os.getenv('CVAT_HOST')}"]
+
+
+# Email verification
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_CONFIRM_EMAIL_ON_GET = True
+ACCOUNT_EMAIL_REQUIRED = os.getenv('ACCOUNT_EMAIL_REQUIRED')
+ACCOUNT_EMAIL_VERIFICATION = os.getenv('ACCOUNT_EMAIL_VERIFICATION')
+
+# Email backend settings for Django
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASS = os.getenv('EMAIL_PASS')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+
+# Site ID which is used for the templates (i.e. email invites)
+SITE_ID = os.getenv('SITE_ID')
